13a8f0a32abd76db940fa90b59eb72accdf090f2
Gabriela <gabyg488@gmail.com>
2025-10-29 20:54:35 -0600

    carga

---
+++ Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using OfficeOpenXml;
+using System.ComponentModel;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Northwind
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        [System.Obsolete]
+        protected void Application_Start()
+        {
+            //ExcelPackage.License = new License { Context = LicenseContext.NonCommercial };
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ Migrations/202510292010527_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿namespace Northwind.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        CustomerID = c.String(nullable: false, maxLength: 128),
+                        CompanyName = c.String(),
+                        ContactName = c.String(),
+                        ContactTitle = c.String(),
+                        Address = c.String(),
+                        City = c.String(),
+                        Region = c.String(),
+                        PostalCode = c.String(),
+                        Country = c.String(),
+                        Phone = c.String(),
+                        Fax = c.String(),
+                    })
+                .PrimaryKey(t => t.CustomerID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Customers");
+        }
+    }
+}
